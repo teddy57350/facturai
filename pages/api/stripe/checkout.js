d132048a20bf1f7863a541ed7a03c2985aaaa608
teddy57350 <lallemandteddy@yahoo.fr>
2026-03-31 17:08:03 +0200

    Update checkout.js

--- pages/api/stripe/checkout.js
+++ pages/api/stripe/checkout.js
@@ -6,18 +6,22 @@ export default async function handler(req, res) {
   try {
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
+
       line_items: [
         {
           price: "price_1TGzLJLcn25N5LCHtJGUeLa4",
           quantity: 1,
         },
       ],
-      success_url: `${process.env.NEXT_PUBLIC_URL}/dashboard`,
-      cancel_url: `${process.env.NEXT_PUBLIC_URL}/dashboard`,
+
+      success_url: `${process.env.NEXT_PUBLIC_URL}/?success=true`,
+      cancel_url: `${process.env.NEXT_PUBLIC_URL}/?canceled=true`,
     });
 
-    return res.status(200).json({ url: session.url });
+    res.status(200).json({ url: session.url });
+
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    console.error("STRIPE ERROR:", error);
+    res.status(500).json({ error: error.message });
   }
 }
