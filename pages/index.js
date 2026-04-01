bf76db2fc8911e7aa2b771cbf5d0a94c55978a5a
teddy57350 <lallemandteddy@yahoo.fr>
2026-04-01 04:39:29 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../../lib/supabase";
 
 export default function Home() {
   const router = useRouter();
