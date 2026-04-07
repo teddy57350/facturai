35280ea073e18266b38c63e03044ce0d3e6be14c
teddy57350 <lallemandteddy@yahoo.fr>
2026-04-07 04:52:14 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
+import { createClient } from "@supabase/supabase-js";
+ 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+ 
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 const FREE_LIMIT = 10;
 
